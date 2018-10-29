6e127a0eb1db5c7e74ffa6214755f4fa46a0d62c
RYDKINGS <borjareyesdominguez2@gmail.com>
2018-10-29 13:10:55 +0000

    Update ETS2.java

--- ETS-2/src/ets/pkg2/ETS2.java
+++ ETS-2/src/ets/pkg2/ETS2.java
@@ -15,8 +15,7 @@ public class ETS2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /*Esto es una prueba para el repositorio de github*/
-        
+     
         System.out.println("prueba completada");
     }
     
